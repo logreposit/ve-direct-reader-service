9de2d738884650299a4fb0cbce92083cfc9deaf2
Dominic Miglar <dominic.miglar@netunix.at>
2021-12-25 18:43:02 +0100

    LogrepositIngressDataMapperTests: added tests for all possible string representations

--- src/test/kotlin/com/logreposit/vedirectreaderservice/services/logreposit/mappers/LogrepositIngressDataMapperTests.kt
+++ src/test/kotlin/com/logreposit/vedirectreaderservice/services/logreposit/mappers/LogrepositIngressDataMapperTests.kt
@@ -137,6 +137,83 @@ class LogrepositIngressDataMapperTests {
         assertThat((fields[11] as IntegerField).value).isEqualTo(0)
     }
 
+    @Test
+    fun `test map from VeDirectReadings to string representation fields`() {
+        val now = Instant.now()
+
+        val ingressData = LogrepositIngressDataMapper(false).toLogrepositIngressDto(
+            date = now,
+            address = "1",
+            data = listOf(
+                VeDirectNumberReading(field = VeDirectField.OR, value = 2),
+                VeDirectNumberReading(field = VeDirectField.CS, value = 4),
+                VeDirectNumberReading(field = VeDirectField.ERR, value = 33),
+                VeDirectNumberReading(field = VeDirectField.MODE, value = 253),
+                VeDirectNumberReading(field = VeDirectField.MPPT, value = 1),
+                VeDirectNumberReading(field = VeDirectField.MON, value = -9)
+            )
+        )
+
+        assertThat(ingressData.readings).hasSize(1)
+        assertThat(ingressData.readings[0].measurement).isEqualTo("data")
+        assertThat(ingressData.readings[0].date).isEqualTo(now)
+        assertThat(ingressData.readings[0].tags).hasSize(1)
+        assertThat(ingressData.readings[0].tags[0].name).isEqualTo("device_address")
+        assertThat(ingressData.readings[0].tags[0].value).isEqualTo("1")
+
+        val fields = ingressData.readings[0].fields
+
+        assertThat(fields).hasSize(12)
+
+        assertThat(fields[0].name).isEqualTo("off_reason")
+        assertThat(fields[0].datatype).isEqualTo(DataType.INTEGER)
+        assertThat((fields[0] as IntegerField).value).isEqualTo(2L)
+
+        assertThat(fields[1].name).isEqualTo("off_reason_str")
+        assertThat(fields[1].datatype).isEqualTo(DataType.STRING)
+        assertThat((fields[1] as StringField).value).isEqualTo("SWITCHED_OFF_POWER_SWITCH")
+
+        assertThat(fields[2].name).isEqualTo("operation_state")
+        assertThat(fields[2].datatype).isEqualTo(DataType.INTEGER)
+        assertThat((fields[2] as IntegerField).value).isEqualTo(4L)
+
+        assertThat(fields[3].name).isEqualTo("operation_state_str")
+        assertThat(fields[3].datatype).isEqualTo(DataType.STRING)
+        assertThat((fields[3] as StringField).value).isEqualTo("ABSORPTION")
+
+        assertThat(fields[4].name).isEqualTo("error_code")
+        assertThat(fields[4].datatype).isEqualTo(DataType.INTEGER)
+        assertThat((fields[4] as IntegerField).value).isEqualTo(33L)
+
+        assertThat(fields[5].name).isEqualTo("error_code_str")
+        assertThat(fields[5].datatype).isEqualTo(DataType.STRING)
+        assertThat((fields[5] as StringField).value).isEqualTo("INPUT_VOLTAGE_TOO_HIGH")
+
+        assertThat(fields[6].name).isEqualTo("device_mode")
+        assertThat(fields[6].datatype).isEqualTo(DataType.INTEGER)
+        assertThat((fields[6] as IntegerField).value).isEqualTo(253L)
+
+        assertThat(fields[7].name).isEqualTo("device_mode_str")
+        assertThat(fields[7].datatype).isEqualTo(DataType.STRING)
+        assertThat((fields[7] as StringField).value).isEqualTo("HIBERNATE")
+
+        assertThat(fields[8].name).isEqualTo("mppt_tracker_operation_mode")
+        assertThat(fields[8].datatype).isEqualTo(DataType.INTEGER)
+        assertThat((fields[8] as IntegerField).value).isEqualTo(1L)
+
+        assertThat(fields[9].name).isEqualTo("mppt_tracker_operation_mode_str")
+        assertThat(fields[9].datatype).isEqualTo(DataType.STRING)
+        assertThat((fields[9] as StringField).value).isEqualTo("VOLTAGE_OR_CURRENT_LIMITED")
+
+        assertThat(fields[10].name).isEqualTo("dc_monitor_mode")
+        assertThat(fields[10].datatype).isEqualTo(DataType.INTEGER)
+        assertThat((fields[10] as IntegerField).value).isEqualTo(-9L)
+
+        assertThat(fields[11].name).isEqualTo("dc_monitor_mode_str")
+        assertThat(fields[11].datatype).isEqualTo(DataType.STRING)
+        assertThat((fields[11] as StringField).value).isEqualTo("SOLAR_CHARGER")
+    }
+
     private fun sampleVeDirectReadings() = listOf(
         VeDirectNumberReading(field = VeDirectField.V, value = 24528),
         VeDirectNumberReading(field = VeDirectField.I, value = -2521),
