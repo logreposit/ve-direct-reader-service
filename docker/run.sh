17fbf6feebb13e441cb2cfffffd6abc0899d5e0c
Dominic Miglar <dominic.miglar@netunix.at>
2022-01-14 15:01:25 +0100

    run.sh: reduce jvm xmx to 128m

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/ve-direct-reader-service/app.jar
+java -Xmx128m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/ve-direct-reader-service/app.jar
