d913d0ed274e3eb4774a23e5f9cfa45869f10bc4
Dominic Miglar <dominic.miglar@netunix.at>
2022-03-21 16:50:41 +0100

    removed memory settings (let docker decide depending on mem_limit)

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-java -Xmx128m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/ve-direct-reader-service/app.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/ve-direct-reader-service/app.jar
